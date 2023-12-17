c49fc043cd76ec81a6f5fd5095f659b0b0ce3caa
Tik Tok Api <webqart@icloud.com>
2023-12-17 15:20:44 +0300

    Create api.py
    # tiktokapi
    tiktok x-ladon, tiktok x-argus, tiktok x-gorgon, tiktok x-khronos, tiktok device register, tiktok api
    
    # TikTok Private API
    
    # Tiktok (Source Code)
    * Private API
    * X-Gorgon
    * X-Khronos 
    * X-Argus 
    * X-Ladon 
    * TTEncrypt
    * Xlog
    
    ## tiktok API
    
    1. login to the user by email and username
    2. register new device
    3. receive information of the user through user_id
    4. receive information about your authorized profile
    5. get list of user's subscribers
    6. get list of user's subscriptions
    7. receive user posts (publications)
    8. receive comments for posts
    9. like comments
    10. like posts
    11. subscribe to user
    12. search
    13. search hashtags
    14. search post hashtags
    15. search users
    16. download Post By Video Id
    17. download tiktok video without watermark
    18. follow user
    19. unfollow user
    20. create account
    21. resolve captcha
    22. create accounts
    
    ## Contact Telegram [@bloodyev] 
    https://t.me/bloodyev

---
+++ api.py
@@ -0,0 +1,41 @@
+# tiktokapi
+tiktok x-ladon, tiktok x-argus, tiktok x-gorgon, tiktok x-khronos, tiktok device register, tiktok api
+
+# TikTok Private API
+
+# Tiktok (Source Code)
+* Private API
+* X-Gorgon
+* X-Khronos 
+* X-Argus 
+* X-Ladon 
+* TTEncrypt
+* Xlog
+
+## tiktok API
+
+1. login to the user by email and username
+2. register new device
+3. receive information of the user through user_id
+4. receive information about your authorized profile
+5. get list of user's subscribers
+6. get list of user's subscriptions
+7. receive user posts (publications)
+8. receive comments for posts
+9. like comments
+10. like posts
+11. subscribe to user
+12. search
+13. search hashtags
+14. search post hashtags
+15. search users
+16. download Post By Video Id
+17. download tiktok video without watermark
+18. follow user
+19. unfollow user
+20. create account
+21. resolve captcha
+22. create accounts
+
+## Contact Telegram [@bloodyev] 
+https://t.me/bloodyev
